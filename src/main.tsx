a2d6e075bb2e208203ce376cd2f0c6be31b79cb9
Eko Mardiatno <ekomardiatno@gmail.com>
2026-01-24 18:22:28 +0700

    chore: update ci/cd workflows

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
